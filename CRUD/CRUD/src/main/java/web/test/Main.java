7f27b82f49c6fb50b318d16f005627adf56ca74e
NikitaVishnevkyy <119518869+NikitaVishnevkyy@users.noreply.github.com>
2023-04-06 10:14:32 +0300

    Add files via upload

---
+++ CRUD/CRUD/src/main/java/web/test/Main.java
@@ -0,0 +1,15 @@
+package web.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import web.config.HibernateConfig;
+import web.dao.HibernateDAO;
+import web.model.User;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HibernateConfig.class);
+        HibernateDAO hibernateDAO = context.getBean(HibernateDAO.class);
+        hibernateDAO.saveUser(new User("Nikita",20,"email111"));
+        context.close();
+    }
+}
